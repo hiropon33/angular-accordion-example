64f6b19df3e4f322b11f929b2a31fd8957cb8c62
Hiroki Mori <mori.ahacraft@gmail.com>
2018-10-16 17:04:03 +0900

    first commit!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('accordion', [
+      transition(':enter', [
+        style({ height: '0', opacity: 0, overflow: 'hidden' }),
+        animate('500ms', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ height: '*', opacity: '1', overflow: 'hidden' }),
+        animate('500ms', style({ height: '0' }))
+      ])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular accordion example';
+  public showDetail: boolean;
+
+  public ngOnInit() {
+    // initialize
+    this.showDetail = false;
+  }
+
+  // event
+  onAccordion($event) {
+    this.showDetail = !this.showDetail;
+  }
 }
